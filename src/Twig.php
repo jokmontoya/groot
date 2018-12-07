a0e0e9613df21cd0031637dd9fa0bc1f0e5ba513
Jaggy Gauran <jaggy@artisan.studio>
2018-12-08 02:38:06 +0800

    Add twig stuff.

---
+++ src/Twig.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace FosterCommerce\Groot;
+
+class Twig
+{
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,10 +2,18 @@
 
 namespace Tests;
 
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
 class ExampleTest extends TestCase
 {
     function test_something()
     {
-        $this->assertTrue(true);
+        $twig = new Environment(
+            new FilesystemLoader(__DIR__ . '/templates'),
+            ['cache' => false]
+        );
+
+        echo $twig->render('index.twig');
     }
 }
