2d5cbcbe99cd7f01ac1c1d6b677cccdf768b2a19
Jaggy Gauran <jaggy@artisan.studio>
2018-12-12 09:16:01 +0800

    Separate the tests from the cli bootstrap.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,24 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use FosterCommerce\Groot\Providers\FilesystemServiceProvider;
+use FosterCommerce\Groot\Providers\TwigServiceProvider;
 use FosterCommerce\Groot\Container;
 
 class TestCase extends BaseTestCase
 {
+    function setup()
+    {
+        $this->configureContainer();
+    }
+
+    private function configureContainer()
+    {
+        $container = new Container;
+
+        $container->register(new TwigServiceProvider);
+        $container->register(new FilesystemServiceProvider);
+
+        Container::setInstance($container);
+    }
 }
